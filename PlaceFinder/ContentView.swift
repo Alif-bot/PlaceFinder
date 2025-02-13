3c0ea82cc9036f947829d05567c3a3be9972e0e5
Md Alif Hossain <infoalif7@gmail.com>
2025-02-13 11:42:21 +0600

    Initial commit

---
+++ PlaceFinder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PlaceFinder
+//
+//  Created by Md Alif Hossain on 13/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PlaceFinder/PlaceFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceFinderApp.swift
+//  PlaceFinder
+//
+//  Created by Md Alif Hossain on 13/2/25.
+//
+
+import SwiftUI
+
+@main
+struct PlaceFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
